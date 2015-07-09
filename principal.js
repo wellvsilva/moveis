d6691b4b83a9bfff505fa423570eade4d0900526
Wellington Viana <wellvsilva.programador@gmail.com>
2015-07-09 11:35:51 -0300

    Inserindo arquivo principal.js

---
+++ principal.js
@@ -0,0 +1,11 @@
+var banners =
+    ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+
+var bannerAtual = 0;
+
+function trocaBanner () {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+}
+
+SetInterval(trocaBanner, 2000);
